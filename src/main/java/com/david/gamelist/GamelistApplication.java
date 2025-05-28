cced5d823ece9d2c07aa1edcef719548da8d6343
David-Simas <thecodesimas@gmail.com>
2025-05-28 15:17:02 -0300

    Projeto criado

---
+++ src/main/java/com/david/gamelist/GamelistApplication.java
@@ -0,0 +1,13 @@
+package com.david.gamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
