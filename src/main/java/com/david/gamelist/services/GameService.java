c3d45d458cff87a690bf6150a6cbb87a3ba83ab2
David-Simas <thecodesimas@gmail.com>
2025-06-07 10:48:55 -0300

    Buscar Game por Id

--- src/main/java/com/david/gamelist/services/GameService.java
+++ src/main/java/com/david/gamelist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.david.gamelist.dto.GameDTO;
 import com.david.gamelist.dto.GameMinDTO;
 import com.david.gamelist.entities.Game;
 import com.david.gamelist.repositories.GameRepository;
@@ -15,10 +17,18 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;		
 	}
 
-}
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+
+}	
